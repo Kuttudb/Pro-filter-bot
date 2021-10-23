f01337f76f04b94852408f192d0ffb124e3d057a
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2021-10-23 15:37:36 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -68,7 +68,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '🔊 Bot Updates 🔊', url=f"{MT_BOT_UPDATES}"
+                                    '📣Share our Group💓', url=f"{MT_BOT_UPDATES}"
                                 )
                         ]
                     ]
